e308120d662b4ca9a36ab3ea8ec643cd8f47f917
Ilyavosky <ilyacortesruiz@gmail.com>
2025-11-25 23:59:51 -0600

    Crear configuración de base de datos

---
+++ src/main/kotlin/infrastructure/config/DatabaseFactory.kt
@@ -0,0 +1,49 @@
+package com.ilya.infrastructure.config
+
+import com.ilya.data.datasource.Albums
+import com.ilya.data.datasource.Artists
+import com.ilya.data.datasource.Songs
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+import io.ktor.server.config.*
+import kotlinx.coroutines.Dispatchers
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
+import org.jetbrains.exposed.sql.transactions.transaction
+
+
+object DatabaseFactory {
+    fun init(config: ApplicationConfig) {
+        val jdbcUrl = config.property("database.jdbcUrl").getString()
+        val username = config.property("database.username").getString()
+        val password = config.property("database.password").getString()
+
+        Database.connect(createHikariDataSource(jdbcUrl, username, password))
+
+        transaction {
+            SchemaUtils.create(Artists, Albums, Songs)
+        }
+    }
+
+    private fun createHikariDataSource(
+        jdbcUrl: String,
+        username: String,
+        password: String
+    ): HikariDataSource {
+        val config = HikariConfig().apply {
+            this.jdbcUrl = jdbcUrl
+            this.username = username
+            this.password = password
+            driverClassName = "org.postgresql.Driver"
+            maximumPoolSize = 10
+            isAutoCommit = false
+            transactionIsolation = "TRANSACTION_REPEATABLE_READ"
+            validate()
+        }
+        return HikariDataSource(config)
+    }
+
+    suspend fun <T> dbQuery(block: suspend () -> T): T =
+        newSuspendedTransaction(Dispatchers.IO) { block() }
+}
